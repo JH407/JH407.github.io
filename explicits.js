edeb44c9aa55a2a11f1f379824ad2c0556d2675a
JJM Hermes <julia.hermes@ru.nl>
2024-09-01 12:01:17 +0200

    Update explicits.js
    Added race question, not sure if it will work as demographic.js uses an additional question

--- explicits.js
+++ explicits.js
@@ -52,6 +52,12 @@ define(['questAPI'], function(Quest){
         type : 'dropdown',
         autoSubmit:false
     });
+
+	API.addQuestionsSet('multiChoice',{
+		inherit: 'basicQ',
+		type: 'selectMulti'
+	});
+
 	
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
@@ -109,6 +115,21 @@ define(['questAPI'], function(Quest){
         stem: 'Wat is uw geboortejaar?',
 	answers: [2006,2005,2004,2003,2002,2001,2000,1999,1998,1997,1996,1995,1994,1993,1992,1991,1990,1989,1988,1987,1986,1985,1984,1983,1982,1981,1980,1979,1978,1977,1976,1975,1974,1973,1972,1971,1970,1969,1968,1967,1966,1965,1964,1963,1962,1961,1960,1959,1958,1957,1956,1955,1954,1953,1952,1951,1950,1949,1948,1947,1946,1945,1944,1943,1942,1941,1940,1939,1938,1937,1936,1935,1934,1933,1932,1931,1930,1929,1928,1927,1926,1925,1924,1923,1922,1921,1920]
     });
+
+API.addQuestionsSet('raceombmulti',{
+		inherit: 'multiChoice',
+		name: 'raceombmulti',
+        stem: "Please select the categories that comprise your race. (Click a category once to select it. Click it again to deselect. You may select as many categories as you wish. When you are finished, click Submit.)",
+        answers: [
+			{text:'American Indian/Alaska Native',value:1},
+			{text:'East Asian',value:2},
+			{text:'South Asian',value:3},
+			{text:'Native Hawaiian or other Pacific Islander',value:4},
+			{text:'Black or African American',value:5},
+			{text:'White',value:6},
+			{text:'Other or Unknown',value:7}
+		]
+	});
 	
     API.addQuestionsSet('education',{
      	inherit : 'basicSelect',
@@ -236,6 +257,10 @@ define(['questAPI'], function(Quest){
                     inherit:'basicPage', 
                     questions: [{inherit:'age'}]
                 },
+	    {
+			inherit: 'basicPage',
+			questions: [{inherit: 'raceombmulti'}]
+		},
 		{
                     inherit:'basicPage', 
                     questions: [{inherit:'education'}]
