467bcf33115e689f04756320ee3e5ff4ebdb0586
JJM Hermes <julia.hermes@ru.nl>
2024-10-13 20:24:45 +0200

    Update explicits.js
    vragen uit sequence weggehaald

--- explicits.js
+++ explicits.js
@@ -105,24 +105,7 @@ define(['questAPI'], function(Quest){
         {
             mixer : 'random', 
             data : [
-                {
-                    mixer : 'random', 
-                    wrapper:true, 
-                    data : [
-                        {
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermBlack'}
-                        },
-                        {
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermWhite'}							
-                        }
-                    ]
-                },
-                {
-                    inherit:'basicPage', 
-                    questions: {inherit:'attributes7'}
-                },
+                
 		{
                     inherit:'basicPage', 
                     questions: [{inherit:'age'}]
