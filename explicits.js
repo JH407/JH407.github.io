7e45d649099898c8edbb7190c093af028e01c300
JJM Hermes <julia.hermes@ru.nl>
2024-10-13 15:58:41 +0200

    Update explicits.js
    eerdere versie van 11 aug opnieuw gecommit

--- explicits.js
+++ explicits.js
@@ -9,9 +9,9 @@ define(['questAPI'], function(Quest){
         noSubmit:false, //Change to true if you don't want to show the submit button.
         header: 'Questionnaire',
         decline: true,
-        declineText: isTouch ? 'Weiger' : 'Weigeren te antwoorden', 
+        declineText: isTouch ? 'Weiger' : 'Wil ik liever niet beantwoorden', 
         autoFocus:true, 
-        progressBar:  'Vraag <%= pagesMeta.number %> van de 4'
+        progressBar:  'Pagina <%= pagesMeta.number %> van 3'
     });
 	
     /**
@@ -23,45 +23,113 @@ define(['questAPI'], function(Quest){
         errorMsg: {
             required: isTouch 
                 ? 'Selecteer een antwoord of klik op \'Weiger\'' 
-                : 'Selecteer een antwoord of klik op \'Weigeren te antwoorden\''
+                : 'Selecteer een antwoord of klik op \'Wil ik liever niet beantwoorden\''
         },
         autoSubmit:'true',
         numericValues:'true',
-        help: '<%= pagesMeta.number < 4 %>',
-        helpText: 'Tip: Voor een snel antwoord klik om je antwoord te selecteren en klik je nogmaals om te verzenden.'
+        help: '<%= pagesMeta.number < 3 %>',
+        helpText: 'Tip: Klik twee maal op uw keuze om snel te antwoorden.'
     });
 
-
-    API.addQuestionsSet('basicText',{
+    API.addQuestionsSet('basicSelect',{
+        inherit :'basicQ',
+        type: 'selectOne'
+    });
+	
+	API.addQuestionsSet('basicText',{
         inherit :'basicQ',
         type: 'text'
     });
 	
-   
-   
+    API.addQuestionsSet('basicDropdown',{
+        inherit :'basicQ',
+        type : 'dropdown',
+        autoSubmit:false
+    });
+	
+    API.addQuestionsSet('therm',{
+        inherit: 'basicSelect',
+        answers: [
+            {text:'10 - Extreem warm', value:10},
+            {text:'9 - Zeer warm', value:9},
+            {text:'8 - Warm', value:8},
+            {text:'7 - Best warm', value:7},
+            {text:'6 - Beetje warm', value:6},
+            {text:'5 - Noch warm noch koud', value:5},
+            {text:'4 - Beetje koud', value:4},
+            {text:'3 - Best koud', value:3},
+            {text:'2 - Koud', value:2},
+            {text:'1 - Zeer koud', value:1},
+            {text:'0 - Extreem koud', value:0}
+        ]
+    });
+
+	
+    /**
+	*Specific questions
+	*/	
+    API.addQuestionsSet('attributes7',{
+        inherit : 'basicSelect',
+        name: 'attributes7',
+        stem: 'Welk statement pas het beste bij u?',
+        answers: [
+            {text:'Ik heb een sterke voorkeur voor <%= global.cisgenderLabels %> boven <%= global.transgenderLabels %>.',value:7},
+            {text:'Ik heb een matige voorkeur voor <%= global.cisgenderLabels %> boven <%= global.transgenderLabels %>.',value:6},
+            {text:'Ik heb een lichte voorkeur voor <%= global.cisgenderLabels %> boven <%= global.transgenderLabels %>.',value:5},
+            {text:'Ik heb tussen <%= global.cisgenderLabels %> en <%= global.transgenderLabels %> geen voorkeur.',value:4},
+            {text:'Ik heb een lichte voorkeur voor <%= global.transgenderLabels %> boven <%= global.cisgenderLabels %>.',value:3},
+            {text:'Ik heb een matige voorkeur voor <%= global.transgenderLabels %> boven <%= global.cisgenderLabels %>.',value:2},
+            {text:'Ik heb een sterke voorkeur voor <%= global.transgenderLabels %> boven <%= global.cisgenderLabels %>.',value:1}
+        ]
+    });
 
     API.addQuestionsSet('age', {
         inherit : 'basicText',
-        name: 'uniek',
-        stem: 'Voer hier de code in die u in de vragenlijst heeft gegenereerd.'
+        name: 'age',
+        stem: 'Hoe oud bent u?'
+    })
+	
+    API.addQuestionsSet('thermBlack',{
+        inherit : 'therm',
+        name: 'Tblack_0to10',
+        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.transgenderLabels %></b>?'
     });
-	
-	 API.addQuestionsSet('age', {
-        inherit : 'basicText',
-        name: 'check',
-        stem: 'Werkt de code zo wel?.'
+
+    API.addQuestionsSet('thermWhite',{
+        inherit : 'therm',
+        name: 'Twhite_0to10',
+        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.cisgenderLabels %></b>?'
     });
-	
-	
+
     API.addSequence([
-	    
-        {  inherit:'basicPage', 
-        questions: [{inherit:'uniek'}] },
-	    
-	     {  inherit:'basicPage', 
-        questions: [{inherit:'check'}] }
+        {
+            mixer : 'random', 
+            data : [
+                {
+                    mixer : 'random', 
+                    wrapper:true, 
+                    data : [
+                        {
+                            inherit:'basicPage', 
+                            questions: {inherit:'thermBlack'}
+                        },
+                        {
+                            inherit:'basicPage', 
+                            questions: {inherit:'thermWhite'}							
+                        }
+                    ]
+                },
+                {
+                    inherit:'basicPage', 
+                    questions: {inherit:'attributes7'}
+                },
+		{
+                    inherit:'basicPage', 
+                    questions: [{inherit:'age'}]
+                }
+            ]
+        }
     ]);
 
     return API.script;
 });
-
