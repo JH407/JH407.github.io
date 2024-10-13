fa906c0031d07873af5f3bd658adef1f021ded09
JJM Hermes <julia.hermes@ru.nl>
2024-10-13 15:54:51 +0200

    Update explicits.js
    poging 4, aanpassing vanuit 3

--- explicits.js
+++ explicits.js
@@ -31,57 +31,14 @@ define(['questAPI'], function(Quest){
         helpText: 'Tip: Voor een snel antwoord klik om je antwoord te selecteren en klik je nogmaals om te verzenden.'
     });
 
-    API.addQuestionsSet('basicSelect',{
-        inherit :'basicQ',
-        type: 'selectOne'
-    });
 
     API.addQuestionsSet('basicText',{
         inherit :'basicQ',
         type: 'text'
     });
 	
-    API.addQuestionsSet('basicDropdown',{
-        inherit :'basicQ',
-        type : 'dropdown',
-        autoSubmit:false
-    });
-	
-    API.addQuestionsSet('therm',{
-        inherit: 'basicSelect',
-        answers: [
-            {text:'10 - Extreem warm', value:10},
-            {text:'9 - Zeer warm', value:9},
-            {text:'8 - Matig warm', value:8},
-            {text:'7 - Enigszins warm', value:7},
-            {text:'6 - Licht warm', value:6},
-            {text:'5 - Noch warm noch koud', value:5},
-            {text:'4 - Licht koud', value:4},
-            {text:'3 - Enigszins koud', value:3},
-            {text:'2 - Matig koud', value:2},
-            {text:'1 - Zeer koud', value:1},
-            {text:'0 - Extreem koud', value:0}
-        ]
-    });
-
-	
-    /**
-	*Specific questions
-	*/	
-    API.addQuestionsSet('attributes7',{
-        inherit : 'basicSelect',
-        name: 'voorkeur',
-        stem: 'Welke uitspraak beschrijft jou het best?',
-        answers: [
-            {text:'Ik heb een sterke voorkeur voor <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:7},
-            {text:'Ik geef een matige voorkeur aan <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:6},
-            {text:'Ik geef een lichte voorkeur aan <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:5},
-            {text:'Ik vind <%= global.rightLabels %> en <%= global.leftLabels %> even leuk.',value:4},
-            {text:'Ik geef een lichte voorkeur aan <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:3},
-            {text:'Ik geef een matige voorkeur aan <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:2},
-            {text:'Ik heb een sterke voorkeur voor <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:1}
-        ]
-    });
+   
+   
 
     API.addQuestionsSet('age', {
         inherit : 'basicText',
@@ -96,18 +53,6 @@ define(['questAPI'], function(Quest){
     });
 	
 	
-    API.addQuestionsSet('thermBlack',{
-        inherit : 'therm',
-        name: 'progressief_0to10',
-        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.leftLabels %></b>?'
-    });
-
-    API.addQuestionsSet('thermWhite',{
-        inherit : 'therm',
-        name: 'conservatiefs_0to10',
-        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.rightLabels %></b>?'
-    });
-
     API.addSequence([
 	    
         {  inherit:'basicPage', 
