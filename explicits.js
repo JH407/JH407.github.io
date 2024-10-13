c39113d0caa0b1db8b827f6e7314b700166b0ec8
JJM Hermes <julia.hermes@ru.nl>
2024-10-13 15:49:46 +0200

    Update explicits.js
    poging 3

--- explicits.js
+++ explicits.js
@@ -9,9 +9,9 @@ define(['questAPI'], function(Quest){
         noSubmit:false, //Change to true if you don't want to show the submit button.
         header: 'Questionnaire',
         decline: true,
-        declineText: isTouch ? 'Weiger' : 'Wil ik liever niet beantwoorden', 
+        declineText: isTouch ? 'Weiger' : 'Weigeren te antwoorden', 
         autoFocus:true, 
-        progressBar:  'Pagina <%= pagesMeta.number %> van 13'
+        progressBar:  'Vraag <%= pagesMeta.number %> van de 4'
     });
 	
     /**
@@ -23,60 +23,100 @@ define(['questAPI'], function(Quest){
         errorMsg: {
             required: isTouch 
                 ? 'Selecteer een antwoord of klik op \'Weiger\'' 
-                : 'Selecteer een antwoord of klik op \'Wil ik liever niet beantwoorden\''
+                : 'Selecteer een antwoord of klik op \'Weigeren te antwoorden\''
         },
         autoSubmit:'true',
         numericValues:'true',
-        help: '<%= pagesMeta.number < 14 %>',
-        helpText: 'Tip: Klik twee maal op uw keuze om snel te antwoorden.'
+        help: '<%= pagesMeta.number < 4 %>',
+        helpText: 'Tip: Voor een snel antwoord klik om je antwoord te selecteren en klik je nogmaals om te verzenden.'
     });
-
-	API.addQuestionsSet('singleChoice',{
-		inherit: 'basicQ',
-		type: 'selectOne', 
-		
-	});
 
     API.addQuestionsSet('basicSelect',{
         inherit :'basicQ',
         type: 'selectOne'
     });
-	
-	API.addQuestionsSet('basicText',{
+
+    API.addQuestionsSet('basicText',{
         inherit :'basicQ',
         type: 'text'
     });
-	
-	API.addQuestionsSet('singleChoicedrop',{
-		inherit: 'basicQ',
-		autoSubmit:false,
-		type: 'dropdown'
-	});
 	
     API.addQuestionsSet('basicDropdown',{
         inherit :'basicQ',
         type : 'dropdown',
         autoSubmit:false
     });
+	
+    API.addQuestionsSet('therm',{
+        inherit: 'basicSelect',
+        answers: [
+            {text:'10 - Extreem warm', value:10},
+            {text:'9 - Zeer warm', value:9},
+            {text:'8 - Matig warm', value:8},
+            {text:'7 - Enigszins warm', value:7},
+            {text:'6 - Licht warm', value:6},
+            {text:'5 - Noch warm noch koud', value:5},
+            {text:'4 - Licht koud', value:4},
+            {text:'3 - Enigszins koud', value:3},
+            {text:'2 - Matig koud', value:2},
+            {text:'1 - Zeer koud', value:1},
+            {text:'0 - Extreem koud', value:0}
+        ]
+    });
 
-	API.addQuestionsSet('multiChoice',{
-		inherit: 'basicQ',
-		type: 'selectMulti'
-	});
+	
+    /**
+	*Specific questions
+	*/	
+    API.addQuestionsSet('attributes7',{
+        inherit : 'basicSelect',
+        name: 'voorkeur',
+        stem: 'Welke uitspraak beschrijft jou het best?',
+        answers: [
+            {text:'Ik heb een sterke voorkeur voor <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:7},
+            {text:'Ik geef een matige voorkeur aan <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:6},
+            {text:'Ik geef een lichte voorkeur aan <%= global.rightLabels %> boven <%= global.leftLabels %>.',value:5},
+            {text:'Ik vind <%= global.rightLabels %> en <%= global.leftLabels %> even leuk.',value:4},
+            {text:'Ik geef een lichte voorkeur aan <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:3},
+            {text:'Ik geef een matige voorkeur aan <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:2},
+            {text:'Ik heb een sterke voorkeur voor <%= global.leftLabels %> boven <%= global.rightLabels %>.',value:1}
+        ]
+    });
 
-	API.addQuestionsSet('basicText',{
-        inherit :'basicQ',
-        name: 'code',
-	stem: 'Voer nu de unieke code in die u bij de vragenlijst heeft gegenereerd.',
+    API.addQuestionsSet('age', {
+        inherit : 'basicText',
+        name: 'uniek',
+        stem: 'Voer hier de code in die u in de vragenlijst heeft gegenereerd.'
+    });
+	
+	 API.addQuestionsSet('age', {
+        inherit : 'basicText',
+        name: 'check',
+        stem: 'Werkt de code zo wel?.'
+    });
+	
+	
+    API.addQuestionsSet('thermBlack',{
+        inherit : 'therm',
+        name: 'progressief_0to10',
+        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.leftLabels %></b>?'
+    });
+
+    API.addQuestionsSet('thermWhite',{
+        inherit : 'therm',
+        name: 'conservatiefs_0to10',
+        stem: 'Hoe warm of koud voel je je ten opzichte van <b><%= global.rightLabels %></b>?'
     });
 
     API.addSequence([
-	    {
-			inherit: 'basicPage',
-			questions: [{inherit: 'code'}]
-		},
-	   
+	    
+        {  inherit:'basicPage', 
+        questions: [{inherit:'uniek'}] },
+	    
+	     {  inherit:'basicPage', 
+        questions: [{inherit:'check'}] }
     ]);
 
     return API.script;
 });
+
