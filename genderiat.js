a7d47fe4f5cd381261a8a2b09d6f0015eb4e8bf0
JJM Hermes <julia.hermes@ru.nl>
2024-10-15 16:26:38 +0200

    Update genderiat.js

--- genderiat.js
+++ genderiat.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
+define(['pipAPI','https://github.com/JH407/JH407.github.io/blob/45bf90719aca0fec7ddfa33999de6f13524002ba/iat10.js'], function(APIConstructor, iatExtension){
     let API = new APIConstructor();
     let global = API.getGlobal();
 
